495905dcd1da357fdc3fd0a563ee0cbdc9f5d539
ezeguaimas <ezequiel.guaimas@gmail.com>
2023-06-03 18:10:41 -0300

    Cotizador actualizado - Genera PDF

---
+++ app.js
@@ -0,0 +1,93 @@
+function calcular() {
+  const aporte1 = parseFloat(document.getElementById("aporte1").value) || 0;
+  const aporte2 = parseFloat(document.getElementById("aporte2").value) || 0;
+  const aporte3 = parseFloat(document.getElementById("aporte3").value) || 0;
+  const aporte4 = parseFloat(document.getElementById("aporte4").value) || 0;
+  let adultos = document.getElementById("adultos").value.trim();
+  let menores = document.getElementById("menores").value.trim();
+
+  // Valores de los planes
+  const corpAdulto = 3310; //2760;
+  const corpMenor = 3310; //2760;
+  const vipAdulto = 6240; //5200;
+  const vipMenor = 4680; //3900;
+
+  let sueldoBruto = (aporte1 + aporte2 + aporte3 + aporte4) / 0.03;
+
+  const montoMaximo = 642142.18;
+
+  if (sueldoBruto > montoMaximo) {
+    sueldoBruto = montoMaximo;
+  }
+
+  if (menores === "") {
+    menores = 0;
+  }
+
+  if (adultos === "") {
+    const mensajeError = document.getElementById("mensaje-error");
+    mensajeError.textContent = "Ingrese valores para adultos y menores";
+    setTimeout(function () {
+      mensajeError.textContent = "";
+    }, 3000);
+    return;
+  }
+
+  const aporteOS = sueldoBruto * 0.0765;
+
+  const aporteTotal = aporteOS;
+  const corpDif = aporteTotal - (corpAdulto * adultos + corpMenor * menores);
+  const vipDif = aporteTotal - (vipAdulto * adultos + vipMenor * menores);
+  let renderCorp = 0;
+  let renderVip = 0;
+
+  if (corpDif > 0) {
+    renderCorp = "No Paga Diferencia";
+  } else {
+    renderCorp = Math.abs(corpDif);
+  }
+
+  if (vipDif > 0) {
+    renderVip = "No Paga Diferencia";
+  } else {
+    renderVip = Math.abs(vipDif);
+  }
+
+  // Mostramos los resultados en el HTML
+  document.getElementById("sueldo_bruto").textContent = sueldoBruto.toFixed(2);
+  document.getElementById("aporte_os").textContent = aporteOS.toFixed(2);
+  document.getElementById("aporte_total").textContent = aporteTotal.toFixed(2);
+  document.getElementById("dif_corp").textContent =
+    typeof renderCorp === "number" ? renderCorp.toFixed(2) : renderCorp;
+  document.getElementById("dif_vip").textContent =
+    typeof renderVip === "number" ? renderVip.toFixed(2) : renderVip;
+
+  mostrarResultados();
+}
+
+function mostrarResultados() {
+  document.getElementById("resultados").style.display = "block";
+}
+
+function ocultarResultados() {
+  document.getElementById("resultados").style.display = "none";
+}
+
+const borrarBtn = document.getElementById("borrar");
+borrarBtn.addEventListener("click", function () {
+  document.getElementById("aporte1").value = "";
+  document.getElementById("aporte2").value = "";
+  document.getElementById("aporte3").value = "";
+  document.getElementById("aporte4").value = "";
+  document.getElementById("adultos").value = "";
+  document.getElementById("menores").value = "";
+  document.getElementById("sueldo_bruto").textContent = "";
+  document.getElementById("aporte_os").textContent = "";
+  document.getElementById("aporte_total").textContent = "";
+  document.getElementById("dif_corp").textContent = "";
+  document.getElementById("dif_vip").textContent = "";
+  calcular();
+  ocultarResultados();
+});
+const botonCalcular = document.getElementById("botonCalcular");
+botonCalcular.addEventListener("click", calcular);
---
+++ cotizacion.js
@@ -0,0 +1,157 @@
+function generarPDF(logoDataURL) {
+  const adultos = document.getElementById("adultos").value;
+  const menores = document.getElementById("menores").value;
+  const nombre = document.getElementById("nombre").valuetoUpperCase();
+  const dif_corp = document.getElementById("dif_corp").innerText;
+  const dif_vip = document.getElementById("dif_vip").innerText;
+
+  const fecha = new Date();
+  const anio = fecha.getFullYear();
+  const mes = fecha.getMonth() + 1;
+  const dia = fecha.getDate();
+
+  const fechaActual = `${dia.toString().padStart(2, "0")}-${mes
+    .toString()
+    .padStart(2, "0")}-${anio}`;
+
+  const docDefinition = {
+    content: [
+      {
+        margin: [10, 10, 20, 10],
+        layout: {
+          defaultBorder: false,
+          paddingTop: function () {
+            return 10;
+          },
+        },
+        table: {
+          widths: ["*"],
+          body: [
+            [
+              {
+                stack: [
+                  {
+                    image: logoDataURL,
+                    alignment: "center",
+                    width: 250,
+                  },
+                  {
+                    text: "Fecha: " + fechaActual,
+                    alignment: "right",
+                    margin: [10, 20, 10, 30],
+                    fontSize: 16,
+                  },
+                  {
+                    text: "COTIZACIÓN",
+                    style: "header",
+                    fontSize: 24,
+                    decoration: "underline",
+                    bold: true,
+                  },
+                  {
+                    stack: [
+                      { text: "Nombre:", fontSize: 14 },
+                      { text: nombre, fontSize: 22, bold: true },
+                    ],
+                    style: "subheader",
+                    margin: [10, 20, 10, 30],
+                  },
+                  {
+                    text: "PLAN: CORPORATIVO",
+                    style: "subheader",
+                    margin: [10, 10, 10, 10],
+                    fontSize: 18,
+                    bold: true,
+                  },
+                  {
+                    text: "Diferencia a Pagar $: " + dif_corp,
+                    margin: [20, 0],
+                    fontSize: 18,
+                    bold: true,
+                  },
+                  {
+                    text:
+                      "Observaciones: Ingresan " +
+                      adultos +
+                      " Adultos, " +
+                      menores +
+                      " menor/es.",
+                    margin: [40, 10],
+                    fontSize: 16,
+                  },
+                  {
+                    text: "PLAN: VIP",
+                    style: "subheader",
+                    margin: [10, 20, 10, 10],
+                    fontSize: 18,
+                    bold: true,
+                  },
+                  {
+                    text: "Diferencia a Pagar $: " + dif_vip,
+                    margin: [20, 0],
+                    fontSize: 18,
+                    bold: true,
+                  },
+                  {
+                    text:
+                      "Observaciones: Ingresan " +
+                      adultos +
+                      " Adultos, " +
+                      menores +
+                      " menor/es.",
+                    margin: [40, 10],
+                    fontSize: 16,
+                  },
+                  {
+                    text: "Precios sujetos a aumentos y/o modificaciones de la Superintendencia de Salud de la Nación.",
+                    style: "footer",
+                    margin: [10, 220, 10, 20],
+                    fontSize: 14,
+                  },
+                ],
+              },
+            ],
+          ],
+        },
+      },
+    ],
+    styles: {
+      header: {
+        bold: true,
+        alignment: "center",
+        margin: [10, 10],
+      },
+      subheader: {
+        bold: true,
+        margin: [0, 10],
+      },
+      footer: {
+        fontSize: 10,
+        italic: true,
+        alignment: "justify",
+      },
+    },
+    pageMargins: [40, 40, 20, 40],
+  };
+
+  pdfMake.createPdf(docDefinition).download("cotizacion_" + nombre + ".pdf");
+}
+
+const botonGenerarPDF = document.getElementById("botonGenerarPDF");
+botonGenerarPDF.addEventListener("click", function () {
+  const logoURL = "Img/mgLogo.png";
+
+  fetch(logoURL)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        const logoDataURL = reader.result;
+        generarPDF(logoDataURL);
+      };
+      reader.readAsDataURL(blob);
+    })
+    .catch((error) => {
+      console.error("Error al cargar la imagen:", error);
+    });
+});
